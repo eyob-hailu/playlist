24773aa5c1072bb74be9502b33b0a9e8d40be475
Eyob Hailu <107069754+eyob-hailu@users.noreply.github.com>
2024-09-17 13:13:40 +0300

    Update server.js
    Commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const songRoutes = require("./routes/routes"); // Ensure this path is correct
+const songRoutes = require("./routes/routes");
 const statRoute = require("./routes/statroute");
 
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    //origin: 'http://localhost:3000',  // Uncomment and set your frontend URL
+    //origin: 'http://localhost:3000',
     credentials: true,
   })
 );
